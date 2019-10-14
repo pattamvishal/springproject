11861dad5f0db797e3f29716641b22dfc3937306
pattam vishal kumar <pattamvishal@gmail.com>
2019-10-14 23:45:59 +0530

    we added comments and made code look good and removed unwanted spaces and formated the code

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -3,9 +3,16 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	// add new fields for emailAddress and team
 	private String emailAddress;
 	private String team;
 	
+		
+	// create a no-arg constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
 	
 	public String getEmailAddress() {
 		return emailAddress;
@@ -13,7 +20,6 @@ public class CricketCoach implements Coach {
 
 	public void setEmailAddress(String emailAddress) {
 		System.out.println("CricketCoach: inside setter method - setEmailAddress");
-
 		this.emailAddress = emailAddress;
 	}
 
@@ -23,15 +29,9 @@ public class CricketCoach implements Coach {
 
 	public void setTeam(String team) {
 		System.out.println("CricketCoach: inside setter method - setTeam");
-
 		this.team = team;
 	}
 
-	// create a no-arg constructor
-	public CricketCoach() {
-		System.out.println("CricketCoach: inside no-arg constructor");
-	}
-	
 	// our setter method
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside setter method - setFortuneService");
